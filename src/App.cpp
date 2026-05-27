1c5a33e9638679e14e95d534129f54c20d7d985c
DashieDev <lamtinh.asian@gmail.com>
2026-05-28 01:06:34 +0700

    window setup

--- src/App.cpp
+++ src/App.cpp
@@ -1,13 +1,33 @@
 #include "App.hpp"
+#include <GLFW/glfw3.h>
 
-void App::initVulkan() {
-    
+void App::initWindow() {
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    this->window = glfwCreateWindow(WINDOW_W, WINDOW_H, "Vulkan Test", NULL, NULL);
 }
 
-void App::renderLoop() {
+void App::initVulkan() {
 
+}
+
+void App::mainLoop() {
+    while (!glfwWindowShouldClose(this->window)) {
+        processWindowInput();
+        glfwPollEvents();
+    }
+}
+
+void App::processWindowInput() {
+    if(glfwGetKey(this->window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(this->window, true);
 }
 
 void App::cleanup() {
+    glfwDestroyWindow(window);
 
+    glfwTerminate();
 }
--- src/App.hpp
+++ src/App.hpp
@@ -1,10 +1,23 @@
+#include <GLFW/glfw3.h>
+
 class App {
 public:
     inline void run() {
+        this->initWindow();
         this->initVulkan();
-        this->renderLoop();
+        this->mainLoop();
         this->cleanup();
     }
 private:
-    void initVulkan(); void renderLoop(); void cleanup();
+    const int WINDOW_W = 800;
+    const int WINDOW_H = 600;
+    GLFWwindow* window;
+
+    void initWindow();
+    void initVulkan(); 
+    
+    void mainLoop();
+    void processWindowInput(); 
+    
+    void cleanup();
 };
